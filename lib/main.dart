8394abafef7215c9e7009d5a7a359e27d83121d9
VoyagerYoshida <yoshida@ss.cs.osakafu-u.ac.jp>
2020-12-04 11:35:13 +0900

    app : Constellation Detecter

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:first_app/next_page.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final String _title_text = '星座検出 App';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: '>>> Home Page of Voyager'),
+      home: MyHomePage(title: _title_text),
     );
   }
 }
@@ -29,40 +33,126 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int num = 6;
+  var scales = [5.5, 6.0, 7.0, 7.0, 20.0, 6.0];
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      if (_counter == num) {
+        _counter = 0;
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      if (_counter == -1) {
+        _counter = num - 1;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: GradientAppBar(
         title: Text(widget.title),
+        backgroundColorStart: const Color(0xffe4a972),
+        backgroundColorEnd: const Color(0xff9941d8),
         actions: <Widget>[
           Icon(Icons.add),
           Icon(Icons.share),
         ],
       ),
+
       body: Center(
-        child: RaisedButton(
-          child: Text('次へ'),
-          onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => NextPage(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'TestImage  No.' + _counter.toString().padLeft(4, "0"),
+              style: TextStyle(
+                fontSize: 20,
               ),
-            );
-          },
+            ),
+
+            Image.asset(
+                'images/original/test_not_resized' + _counter.toString().padLeft(4, "0") + '.jpg',
+                scale: scales[_counter],
+            ),
+            // Image.asset('images/original/DSCF0012.jpeg', scale: 18),
+
+            RaisedButton(
+              child: Text('検出結果を表示'),
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NextPage(_counter),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+
+      floatingActionButton: Container(
+        width: 300.0,
+        height: 100.0,
+        padding: EdgeInsets.only(right: 40.0),
+        // margin: EdgeInsets.only(top:20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              heroTag: "Back",
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight,
+                    colors: const [
+                      Color(0xffe4a972),
+                      Color(0xff9941d8),
+                    ],
+                  )
+                ),
+                child: Icon(Icons.arrow_back_outlined),
+              ),
+            ),
+
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              heroTag: "Forward",
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      begin: FractionalOffset.topLeft,
+                      end: FractionalOffset.bottomRight,
+                      colors: const [
+                        Color(0xffe4a972),
+                        Color(0xff9941d8),
+                      ],
+                    )
+                ),
+                child: Icon(Icons.arrow_forward_outlined),
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,20 +1,49 @@
-
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
+
 
 class NextPage extends StatelessWidget {
+  NextPage(this._counter);
+  int _counter;
+  final String constellation = 'オリオン座';
+  var scales = [2.0, 1.0, 1.0, 1.0, 1.0, 1.0];
+  var prob = [0.7909, 0.6462, 0.6246, 0.7868, 0.6528, 0.8916];
+  var iou = [0.7600, 0.5995, 0.7136, 0.5344, 0.5599, 0.3338];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('>>> Next Page'),
+      appBar: GradientAppBar(
+        title: Text("検出結果"),
+        backgroundColorStart: const Color(0xff9941d8),
+        backgroundColorEnd: const Color(0xff00008B),
         actions: <Widget>[
           Icon(Icons.add),
           Icon(Icons.share),
         ],
       ),
-      body: Container(
-        height: double.infinity,
-        color: Colors.cyanAccent,
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("星座名"),
+            Text(
+              constellation,
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+
+            Image.asset(
+                'images/detected/outputimg' + _counter.toString().padLeft(4, "0") + '.png',
+                scale: scales[_counter],
+            ),
+
+            Text("probability = " + prob[_counter].toString() + " %"),
+            Text("IoU = " + iou[_counter].toString() + " %"),
+          ],
+        ),
       ),
     );
   }
